58f7b3a3fc4bc73159063466b44eba315b0c49a1
Suleiman Odetoro <odetoro75@gmail.com>
2024-10-23 08:10:32 +0100

    Modify config file to allow images from certain sites

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['images.unsplash.com'], // Add Unsplash domain to the list
+      domains: ['images.unsplash.com','unsplash.com'], // Add Unsplash domain to the list
     },
   };
   
